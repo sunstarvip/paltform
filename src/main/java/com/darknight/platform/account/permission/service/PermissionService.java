3e5b01d25599ad3fb50725bd4aed6fbdcfd62c39
DarKnight <songqingxin@vip.qq.com>
2015-04-01 13:48:55 +0800

    在core包中提供公共Service类后，调整权限类业务层实现，抽出公共方法，简洁代码实现

--- src/main/java/com/darknight/platform/account/permission/service/PermissionService.java
+++ src/main/java/com/darknight/platform/account/permission/service/PermissionService.java
@@ -1,90 +1,19 @@
 package com.darknight.platform.account.permission.service;
 
+import com.darknight.core.base.service.BaseService;
 import com.darknight.platform.account.permission.entity.Permission;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 /**
  * Created by DarKnight on 2014/4/26 0026.
  */
-public interface PermissionService {
+public interface PermissionService extends BaseService<Permission, String> {
 
-    //通用方法区域 Begin
-    public void flush();
 
-    public Permission save(Permission permission);
-
-    public List<Permission> save(List<Permission> permissionList);
-
-    public void realDelete(String permissionId);
-
-    public void realDelete(List<String> idList);
-
-    public void realDelete(Permission permission);
-
-    public void realDeleteInBatch(List<Permission> permissionList);
-
-    public void realDeleteAll();
-
-    public long count();
-
-    public boolean exists(String permissionId);
-
-    public Permission find(String permissionId);
-
-    public List<Permission> find(List<String> idList);
-
-    public List<Permission> findAll();
-
-    public Page<Permission> findAll(Pageable page);
-
-    public List<Permission> findAll(Sort sort);
-
-    /**
-     * 查询所有未逻辑删除的Permission权限对象
-     * @return
-     */
-    public List<Permission> findAllVisible();
-
-    /**
-     * 删除该权限ID下的Permission权限对象
-     * 逻辑删除
-     * @param permissionId 权限ID
-     */
-    public void delete(String permissionId);
-
-    /**
-     * 根据传入权限ID, 批量删除Permission权限对象
-     * 逻辑删除
-     * @param idList 权限ID列表
-     */
-    public void delete(List<String> idList);
-
-    /**
-     * 删除Permission权限对象
-     * 逻辑删除
-     * @param permission 权限对象
-     */
-    public void delete(Permission permission);
-
-    /**
-     * 批量删除Permission权限对象
-     * 逻辑删除
-     * @param permissionList 权限对象列表
-     */
-    public void deleteInBatch(List<Permission> permissionList);
-
-    /**
-     * 删除所有的Permission权限对象
-     * 逻辑删除
-     */
-    public void deleteAll();
-    //通用方法区域 End
 
     public Set<Permission> findPermissions(String accountName);
 
--- src/main/java/com/darknight/platform/account/permission/service/impl/PermissionManager.java
+++ src/main/java/com/darknight/platform/account/permission/service/impl/PermissionManager.java
@@ -1,6 +1,8 @@
 package com.darknight.platform.account.permission.service.impl;
 
+import com.darknight.core.base.dao.BaseJpaDao;
 import com.darknight.core.base.entity.DefaultEntity;
+import com.darknight.core.base.service.impl.BaseManager;
 import com.darknight.platform.account.permission.dao.PermissionDao;
 import com.darknight.platform.account.permission.entity.Permission;
 import com.darknight.platform.account.permission.service.PermissionService;
@@ -11,292 +13,27 @@ import org.hibernate.criterion.Restrictions;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by DarKnight on 2014/4/26 0026.
  */
 @Service
 @Transactional(readOnly = true)
-public class PermissionManager implements PermissionService {
+public class PermissionManager extends BaseManager<Permission, String> implements PermissionService {
     private PermissionDao permissionDao;
 
     @Resource
-    public void setPermissionDao(PermissionDao permissionDao) {
-        this.permissionDao = permissionDao;
+    public void setBaseDao(BaseJpaDao<Permission, String> baseDao) {
+        super.setBaseDao(baseDao);
+        this.permissionDao = (PermissionDao)baseDao;
     }
-
-    //通用方法区域 Begin
-    /**
-     * 推送缓存中的数据操作至数据库
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void flush() {
-        permissionDao.flush();
-    }
-
-    /**
-     * 保存Permission权限对象
-     * @param permission
-     * @return
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public Permission save(Permission permission) {
-        return permissionDao.saveAndFlush(permission);
-    }
-
-    /**
-     * 批量保存Permission权限对象
-     * @param permissionList
-     * @return
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public List<Permission> save(List<Permission> permissionList) {
-        permissionList = permissionDao.save(permissionList);
-        flush();
-        return permissionList;
-    }
-
-    /**
-     * 删除该权限ID下的Permission权限对象
-     * @param permissionId
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void realDelete(String permissionId) {
-        permissionDao.delete(permissionId);
-        flush();
-    }
-
-    /**
-     * 根据传入权限ID, 批量删除Permission权限对象
-     * @param idList
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void realDelete(List<String> idList) {
-        for(String permissionId : idList) {
-            permissionDao.delete(permissionId);
-        }
-        flush();
-    }
-
-    /**
-     * 删除Permission权限对象
-     * @param permission
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void realDelete(Permission permission) {
-        permissionDao.delete(permission);
-        flush();
-    }
-
-    /**
-     * 批量删除Permission权限对象
-     * @param permissionList
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void realDeleteInBatch(List<Permission> permissionList) {
-        permissionDao.deleteInBatch(permissionList);
-        flush();
-    }
-
-    /**
-     * 删除所有的Permission权限对象
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void realDeleteAll() {
-        permissionDao.deleteAll();
-        flush();
-    }
-
-    /**
-     * 统计所有Permission权限对象的总数量
-     * @return
-     */
-    @Override
-    public long count() {
-        return permissionDao.count();
-    }
-
-    /**
-     * 根据传入权限ID, 判断该权限是否存在
-     * @param permissionId
-     * @return
-     */
-    @Override
-    public boolean exists(String permissionId) {
-        return permissionDao.exists(permissionId);
-    }
-
-    /**
-     * 根据权限ID, 查询Permission权限对象
-     * @param permissionId
-     * @return
-     */
-    @Override
-    public Permission find(String permissionId) {
-        return permissionDao.getOne(permissionId);
-    }
-
-    /**
-     * 根据传入的权限ID, 批量查询Permission权限对象
-     * @param idList
-     * @return
-     */
-    @Override
-    public List<Permission> find(List<String> idList) {
-        List<Permission> permissionList = permissionDao.findAll(idList);
-        if(permissionList == null) {
-            permissionList = new ArrayList<Permission>();
-        }
-        return permissionList;
-    }
-
-    /**
-     * 查询所有的Permission权限对象
-     * @return
-     */
-    @Override
-    public List<Permission> findAll() {
-        List<Permission> permissionList = permissionDao.findAll();
-        if(permissionList == null) {
-            permissionList = new ArrayList<Permission>();
-        }
-        return permissionList;
-    }
-
-    /**
-     * 分页查询所有的Permission权限对象
-     * @param page
-     * @return
-     */
-    @Override
-    public Page<Permission> findAll(Pageable page) {
-        Page<Permission> permissionPage = permissionDao.findAll(page);
-        return permissionPage;
-    }
-
-    /**
-     * 查询所有的Permission权限对象, 并根据Sort排序规则进行排序
-     * @param sort
-     * @return
-     */
-    @Override
-    public List<Permission> findAll(Sort sort) {
-        List<Permission> permissionList = permissionDao.findAll(sort);
-        if(permissionList == null) {
-            permissionList = new ArrayList<Permission>();
-        }
-        return permissionList;
-    }
-
-    /**
-     * 查询所有未逻辑删除的Permission权限对象
-     * @return
-     */
-    @Override
-    public List<Permission> findAllVisible() {
-        // 创建查询对象
-        Criteria criteria = permissionDao.createCriteria();
-        // 添加查询规则
-        criteria.add(Restrictions.eq("visibleTag", DefaultEntity.VisibleTag.YES));
-        List<Permission> permissionList = criteria.list();
-        return permissionList;
-    }
-
-    /**
-     * 删除该权限ID下的Permission权限对象
-     * 逻辑删除
-     * @param permissionId 权限ID
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void delete(String permissionId) {
-        if(exists(permissionId)) {
-            Permission permission = find(permissionId);
-            permission.setUpdateTime(new Date());
-            permission.setVisibleTag(DefaultEntity.VisibleTag.NO);
-            save(permission);
-        }
-    }
-
-    /**
-     * 根据传入权限ID, 批量删除Permission权限对象
-     * 逻辑删除
-     * @param idList 权限ID列表
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void delete(List<String> idList) {
-        List<Permission> permissionList = find(idList);
-        if(!permissionList.isEmpty()) {
-            for(Permission permission : permissionList) {
-                permission.setUpdateTime(new Date());
-                permission.setVisibleTag(DefaultEntity.VisibleTag.NO);
-            }
-            save(permissionList);
-        }
-    }
-
-    /**
-     * 删除Permission权限对象
-     * 逻辑删除
-     * @param permission 权限对象
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void delete(Permission permission) {
-        permission.setUpdateTime(new Date());
-        permission.setVisibleTag(DefaultEntity.VisibleTag.NO);
-        save(permission);
-    }
-
-    /**
-     * 批量删除Permission权限对象
-     * 逻辑删除
-     * @param permissionList 权限对象列表
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void deleteInBatch(List<Permission> permissionList) {
-        if(!permissionList.isEmpty()) {
-            for(Permission permission : permissionList) {
-                permission.setUpdateTime(new Date());
-                permission.setVisibleTag(DefaultEntity.VisibleTag.NO);
-            }
-            save(permissionList);
-        }
-    }
-
-    /**
-     * 删除所有的Permission权限对象
-     * 逻辑删除
-     */
-    @Override
-    @Transactional(readOnly = false)
-    public void deleteAll() {
-        List<Permission> permissionList = findAllVisible();
-        if(!permissionList.isEmpty()) {
-            for(Permission permission : permissionList) {
-                permission.setUpdateTime(new Date());
-                permission.setVisibleTag(DefaultEntity.VisibleTag.NO);
-            }
-            save(permissionList);
-        }
-    }
-    //通用方法区域 End
 
     /**
      * 根据用户登录账户名称, 查询该用户对应的权限对象
