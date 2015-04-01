9bf9af8ce5e36c90f30abab0e94d54d25b23e662
DarKnight <songqingxin@vip.qq.com>
2015-04-01 13:47:22 +0800

    增加注释

--- src/main/java/com/darknight/platform/account/user/dao/UserDao.java
+++ src/main/java/com/darknight/platform/account/user/dao/UserDao.java
@@ -8,5 +8,10 @@ import com.darknight.platform.account.user.entity.User;
  * Created by DarKnight on 2014/5/22 0022.
  */
 public interface UserDao extends BaseJpaDao<User, String> {
+    /**
+     * 通过账户名查询用户对象
+     * @param accountName 账户名
+     * @return
+     */
     public User findByAccountName(String accountName);
 }
