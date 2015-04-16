bb28b56c66919443030a99e802319bc4fb3d3780
DarKnight <songqingxin@vip.qq.com>
2015-04-16 18:41:10 +0800

    使用Shiro框架，增加用户登录和登出功能，同时实现支持本地登录与通过Shiro的默认Filter进行登录两种方式

--- src/main/java/com/darknight/platform/security/login/controller/LoginController.java
+++ src/main/java/com/darknight/platform/security/login/controller/LoginController.java
@@ -1,10 +1,18 @@
 package com.darknight.platform.security.login.controller;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,7 +22,6 @@ import javax.servlet.http.HttpServletRequest;
  * Created by DarKnight on 2014/5/26 0026.
  */
 @Controller
-@RequestMapping(value = "/login")
 public class LoginController {
     private DefaultWebSecurityManager securityManager;
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
@@ -24,34 +31,98 @@ public class LoginController {
         this.securityManager = securityManager;
     }
 
-//    @RequestMapping(value = "login", method = RequestMethod.POST)
-//    public String login(Model model, HttpServletRequest request) {
-//        String shiroLoginFailure = request.getParameter("shiroLoginFailure");
-//        System.out.print("shiroLoginFailure: " + shiroLoginFailure);
-//
-//        return "platform/login/index";
-//    }
-//
-//    @RequestMapping(value = "login", method = RequestMethod.GET)
-//    public String userLogin(Model model, HttpServletRequest request) {
-//        String shiroLoginFailure = request.getParameter("shiroLoginFailure");
-//        System.out.print("shiroLoginFailure: " + shiroLoginFailure);
-//
-//        return "platform/login/index";
-//    }
-//
-//    @RequestMapping(value = "logout", method = RequestMethod.GET)
-//    public String logout(Model model, HttpServletRequest request) {
-//        System.out.print("logout sucess!");
-//        return "redirect:/platform/account/user/list";
-//    }
+    /**
+     * 跳转至登录页面
+     * 使用本地Controller来控制用户登录
+     * @return
+     */
+    @RequestMapping(value = "loginPage", method = {RequestMethod.GET})
+    public String loginPage() {
+        return "platform/system/loginPage";
+    }
 
+    /**
+     * 通过本地Controller来控制用户登录过程
+     * @param model
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "webLogin", method = RequestMethod.POST)
+    public String webLogin(Model model, HttpServletRequest request) {
+        // 获取当前登录对象
+        Subject loginUser = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(
+                request.getParameter("username"),
+                request.getParameter("password"));
+        // 判断用户是否选择 记住我 选项
+        if(StringUtils.equals("true", request.getParameter("rememberMe"))) {
+            token.setRememberMe(true);
+        }
+
+        try {
+            // 通过Shiro进行用户登录
+            loginUser.login(token);
+            return "redirect:/indexPage";
+        }catch (UnknownAccountException e) {//登录失败
+            e.printStackTrace();
+            log.error("LoginController.webLogin()异常");
+            log.error("用户名不存在");
+            model.addAttribute("errorInfo", "用户名/密码错误");
+        }catch (IncorrectCredentialsException e) {//登录失败
+            e.printStackTrace();
+            log.error("LoginController.webLogin()异常");
+            log.error("用户密码错误");
+            model.addAttribute("errorInfo", "用户名/密码错误");
+        }catch (AuthenticationException e) {//登录失败
+            e.printStackTrace();
+            log.error("LoginController.webLogin()异常");
+            log.error("其他错误");
+            model.addAttribute("errorInfo", "用户名/密码错误");
+        }
+
+        return "platform/system/loginPage";
+    }
+
+    /**
+     * Shiro登录控制，判断用户未登录时进入（GET）并跳转至登录页面，
+     * 用户登录失败时进入（POST），重新转至登录页面，
+     * 并提示用户登录失败原因
+     * @param model
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "login", method = {RequestMethod.GET, RequestMethod.POST})
+    public String login(Model model, HttpServletRequest request) {
+        String errorClassName = (String)request.getAttribute("shiroLoginFailure");
+        if(UnknownAccountException.class.getName().equals(errorClassName)) {
+            log.error("LoginController.login()异常");
+            log.error("用户名不存在");
+            model.addAttribute("errorInfo", "用户名/密码错误");
+        }else if(IncorrectCredentialsException.class.getName().equals(errorClassName)) {
+            log.error("LoginController.login()异常");
+            log.error("用户密码错误");
+            model.addAttribute("errorInfo", "用户名/密码错误");
+        }else if(errorClassName != null) {
+            log.error("LoginController.login()异常");
+            log.error("其他错误");
+            model.addAttribute("errorInfo", "未知错误：" + errorClassName);
+        }
+        return "platform/system/loginShiro";
+    }
+
+    /**
+     * 用户首页
+     * 自定义页面
+     * @param request
+     * @return
+     */
     @RequestMapping(value = "index", method = RequestMethod.GET)
     public String index(HttpServletRequest request) {
         return "platform/system/index";
     }
 
     /**
+     * 用户首页
      * esayUI页面
      * @return
      */
--- src/main/webapp/WEB-INF/views/platform/system/loginPage.jsp
+++ src/main/webapp/WEB-INF/views/platform/system/loginPage.jsp
@@ -1,22 +1,22 @@
 <%--
-  Created by IntelliJ IDEA.
+  本地登录使用的登录页面
   User: DarKnight
   Date: 2014/5/26 0026
   Time: 10:35
-  To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>login</title>
+    <title>用户登录</title>
 </head>
 <body>
-Login Form!!!123
-    <form action="/login.jsp" method="post">
+Login Page!!!123
+    <form action="/webLogin" method="post">
         <br />用户帐号：<input type="text" name="username" id="username" value="" />
         <br />登录密码：<input type="password" name="password" id="password" value="" />
         <br />记住我：<input type="checkbox" name="rememberMe" value="true" />
         <br /><input value="登录" type="submit">
     </form>
+<span>${errorInfo}</span>
 </body>
 </html>
